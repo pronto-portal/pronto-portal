51ca0fc088378dc5ceb9f341ef5b9fa2739b780f
branber <brandonberke@gmail.com>
2023-08-06 01:36:23 -0400

    add starter dashboard template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,40 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+
 export default function Home() {
-  return null;
+  return (
+    <Stack
+      direction="column"
+      width="100%"
+      height="100%"
+      alignItems="center"
+      justifyContent="center"
+      p={2}
+    >
+      <Accordion sx={{ width: "100%" }}>
+        <AccordionSummary>
+          <Typography variant="h5">Translators</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container width="100%" height="100%">
+            <Grid
+              item
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+              xs={2}
+            ></Grid>
+            <Grid item xs={10}></Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
+    </Stack>
+  );
 }
