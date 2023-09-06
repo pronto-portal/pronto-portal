5e4e24879bf6f824443d5938a8e46383190774fe
branber <brandonberke@gmail.com>
2023-09-05 23:27:30 -0400

    add edit icon to card actions

--- components/ConfirmAssignmentForm/ConfirmAssignmentForm.tsx
+++ components/ConfirmAssignmentForm/ConfirmAssignmentForm.tsx
@@ -5,12 +5,15 @@ import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
+import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import { AssignmentFlowForm } from "../../types/PropTypes/AssignmentFlowForm";
 import { FlexRowGridItem } from "../FlexRowGridItem/FlexRowGridItem";
 import { styled } from "@mui/system";
 import { ObjectGridSpread } from "../ObjectGridSpread/ObjectGridSpread";
 import { Address, Claimant, Reminder, User } from "../../types/ObjectTypes";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 
 const FlexCard = styled(Card)({
   flex: 1,
@@ -42,15 +45,26 @@ export const ConfirmAssignmentForm: React.FC<AssignmentFlowForm> = ({
       <FlexRowGridItem xs={2}>
         <FlexCard sx={{ flex: 1 }}>
           <CardHeader title="Address" />
+
           <CardContent>
             <ObjectGridSpread<Address> object={address} />
           </CardContent>
+          <CardActions>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </CardActions>
         </FlexCard>
         <FlexCard>
           <CardHeader title="Claimant" />
           <CardContent>
             <ObjectGridSpread<Claimant> object={claimant} />
           </CardContent>
+          <CardActions>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </CardActions>
         </FlexCard>
       </FlexRowGridItem>
       <FlexRowGridItem xs={2}>
@@ -58,18 +72,29 @@ export const ConfirmAssignmentForm: React.FC<AssignmentFlowForm> = ({
           <CardHeader title="Translator" />
           <CardContent>
             <ObjectGridSpread<User> object={translator} />
-          </CardContent>
+          </CardContent>{" "}
+          <CardActions>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </CardActions>
         </FlexCard>
         <FlexCard>
           <CardHeader title="Date" />
           <CardContent>
             <Typography>{date?.toString()}</Typography>
-          </CardContent>
+          </CardContent>{" "}
+          <CardActions>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </CardActions>
         </FlexCard>
       </FlexRowGridItem>
       <FlexRowGridItem xs={2}>
         <FlexCard>
           <CardHeader title="Reminders" />
+
           <CardContent>
             <Typography>
               Remind claimant: {remindClaimant ? "Yes" : "No"}
@@ -78,6 +103,11 @@ export const ConfirmAssignmentForm: React.FC<AssignmentFlowForm> = ({
               Remind translator: {remindTranslator ? "Yes" : "No"}
             </Typography>
           </CardContent>
+          <CardActions>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+          </CardActions>
         </FlexCard>
       </FlexRowGridItem>
       <Grid item xs={2}>
