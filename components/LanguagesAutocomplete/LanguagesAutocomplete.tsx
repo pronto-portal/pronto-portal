f7f1f9f414b79a59c47c970e9a811108a90d56be
branber <brandonberke@gmail.com>
2023-10-01 22:30:53 -0400

    fix forwarding ref error in console

--- components/LanguagesAutocomplete/LanguagesAutocomplete.tsx
+++ components/LanguagesAutocomplete/LanguagesAutocomplete.tsx
@@ -3,6 +3,7 @@ import { useLanguages } from "../../contextProviders/LanguagesProvider";
 import { Language } from "../../types/ObjectTypes";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import { SxProps, Theme } from "@mui/material";
 
 interface LanguagesAutocompleteProps {
@@ -14,39 +15,52 @@ interface LanguagesAutocompleteProps {
   sx?: SxProps<Theme>;
 }
 
-export const LanguagesAutocomplete: React.FC<LanguagesAutocompleteProps> = ({
-  value,
-  onChange,
-  multiple = false,
-  label,
-  ref,
-  sx,
-}) => {
-  const { languageCodes, getLanguageFromCode } = useLanguages();
+export const LanguagesAutocomplete = React.forwardRef(
+  (
+    {
+      value,
+      onChange,
+      multiple = false,
+      label,
+      sx,
+    }: LanguagesAutocompleteProps,
+    ref: React.ForwardedRef<HTMLInputElement>
+  ) => {
+    const { languageCodes, getLanguageFromCode } = useLanguages();
 
-  return (
-    <Autocomplete
-      ref={ref}
-      multiple={multiple}
-      id="tags-standard"
-      options={languageCodes}
-      getOptionLabel={(option: string) => getLanguageFromCode(option as string)}
-      value={value}
-      sx={sx}
-      onChange={(_, value) => {
-        if (value) {
-          onChange(multiple ? (value as string[]) : (value as string));
+    return (
+      <Autocomplete
+        ref={ref}
+        multiple={multiple}
+        id="tags-standard"
+        options={languageCodes}
+        getOptionLabel={(option: string) =>
+          getLanguageFromCode(option as string)
         }
-      }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="outlined"
-          fullWidth
-          label={label}
-          placeholder={label}
-        />
-      )}
-    />
-  );
-};
+        value={value}
+        sx={sx}
+        onChange={(_, value) => {
+          if (value) {
+            onChange(multiple ? (value as string[]) : (value as string));
+          }
+        }}
+        renderOption={(props, option) => (
+          <li {...props} key={option}>
+            <Typography>{getLanguageFromCode(option as string)} </Typography>
+          </li>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            fullWidth
+            label={label}
+            placeholder={label}
+          />
+        )}
+      />
+    );
+  }
+);
+
+LanguagesAutocomplete.displayName = "LanguagesAutocomplete";
