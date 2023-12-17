6e736831bbbec10ec4ccac83947ff148b0631d57
branber <brandonberke@gmail.com>
2023-12-17 14:22:08 -0500

    replace auth with keys in google places api

--- src/utils/googleapis.ts
+++ src/utils/googleapis.ts
@@ -3,7 +3,7 @@ import { google } from 'googleapis';
 // places api
 const { places } = google.places({
     version: 'v1',
-    auth: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY,
+    key: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY,
 });
 
 export { places };
