a89eecf6e74cad4b5937ff61e2ba24c3247c2d91
branber <brandonberke@gmail.com>
2023-12-17 22:04:26 -0500

    add referrer header to places api

--- src/pages/api/google/places.ts
+++ src/pages/api/google/places.ts
@@ -21,6 +21,7 @@ const GetPlacesPrediction = (req: NextApiRequest, res: NextApiResponse) => {
                 {
                     headers: {
                         'X-Goog-FieldMask': 'places.addressComponents', //'addressComponents,formattedAddress',
+                        Referer: process.env.NODE_ENV === 'production' ? 'https://prontotranslationservices.com/' : 'http://localhost:3000/',
                     },
                 }
             )
@@ -48,7 +49,7 @@ const GetPlacesPrediction = (req: NextApiRequest, res: NextApiResponse) => {
                 resolve();
             })
             .catch((err) => {
-                console.log('placesData err', err);
+                console.log('placesData err', err.errors);
                 res.status(200).json([]);
                 resolve();
             });
