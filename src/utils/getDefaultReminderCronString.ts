42b9dcd103fee9f12f4f3c2b61d77b419674d5fe
branber <brandonberke@gmail.com>
2024-01-28 17:36:54 -0500

    add functionality to get default cron from assn date

---
+++ src/utils/getDefaultReminderCronString.ts
@@ -0,0 +1,19 @@
+import moment from 'moment';
+
+export const getDefaultReminderCronString = (dateString: string) => {
+    // todo: ensure all date string coming into the backend get converted into utc time
+
+    const dateObj = new Date(dateString);
+    const dateTime = moment.utc(dateObj);
+
+    // day before the assignment
+    const day = dateTime.date() - 1 <= 0 ? 1 : dateTime.date() - 1;
+    const month = dateTime.month() + 1;
+    const year = dateTime.year();
+    const hour = dateTime.hour();
+    const minute = dateTime.minute();
+
+    // min hr day_of_month month day_of_week year
+    const cron = `${minute} ${hour} ${day} ${month} ? ${year}`;
+    return cron;
+};
